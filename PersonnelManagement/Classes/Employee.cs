69585feb79528039d32ddd753d6b22dafb3383c0
Frus1 <165252939+Frus1@users.noreply.github.com>
2024-11-22 05:29:30 +0500

    Add files via upload

---
+++ PersonnelManagement/Classes/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelManagement.Classes
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public static ObservableCollection<Employee> Employees { get; set; } = new ObservableCollection<Employee>
+        {
+            new Employee { Name = "Сорокин С.В.", Position = "Менеджер" },
+            new Employee { Name = "Кузнецов А.И.", Position = "Уборщик" }
+        };
+
+    }
+}
+
